cf1cf417ce65d7cf2b5cd9d597196c9923937ba3
zeskord <zeskord@gmail.com>
2020-07-19 22:49:20 +0300

    build

---
+++ client/build/precache-manifest.849f0d2c1c011d5452f1baede8aa1afa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7066fb786f2b35623910e9ce78d91e91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08e2604f0791afb8706a",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "bd06e32335a5c2dac00f",
+    "url": "/static/js/2.6b368c05.chunk.js"
+  },
+  {
+    "revision": "2d71c6fe30c48013e1bec1637fb69610",
+    "url": "/static/js/2.6b368c05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08e2604f0791afb8706a",
+    "url": "/static/js/main.4570ddfc.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
