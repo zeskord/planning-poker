9a05fcfe83ae69241f390c48bb192496bb0b3fc3
zeskord <zeskord@gmail.com>
2021-06-14 00:27:40 +0300

    Добавил особые оценки - кофе и бесконечность

--- client/src/components/PokerCards.js
+++ client/src/components/PokerCards.js
@@ -1,19 +1,47 @@
 import React from "react";
 import { PokerCard } from "./PokerCard";
+import { PokerCardSpecial } from "./PokerCardSpecial";
 
 export const PokerCards = (props) => {
-  var marks = ["0", "0.5", "1", "2", "3", "5", "8", "13", "21", "34", "55", "89"];
+  var marks = [
+    "0",
+    "0.5",
+    "1",
+    "2",
+    "3",
+    "5",
+    "8",
+    "13",
+    "21",
+    "34",
+    "55",
+    "89",
+  ];
 
   return (
     <div>
+      <PokerCardSpecial
+        variant="primary"
+        key="coffee"
+        title="coffee"
+        img="coffee.svg"
+        modalOnSelect={props.modalOnSelect}
+      />
       {marks.map((mark) => (
         <PokerCard
           variant="primary"
-          key = {mark}
+          key={mark}
           title={mark}
           modalOnSelect={props.modalOnSelect}
         />
       ))}
+      <PokerCardSpecial
+        variant="primary"
+        key="infinity"
+        title="infinity"
+        img="infinity.svg"
+        modalOnSelect={props.modalOnSelect}
+      />
     </div>
   );
 };
