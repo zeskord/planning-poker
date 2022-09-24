a2d5089cc9b15a6c4f73b979cc111e419ca88383
zeskord <zeskord@gmail.com>
2022-09-24 21:35:48 +0300

    tg

--- client/src/App.js
+++ client/src/App.js
@@ -42,7 +42,7 @@ export const App = (props) => {
                     <LoginForm setAuthState={setAuthState}/>
                 )}
             <div className="mt-2">
-                <p>{tgUser}</p>
+                <p>o-O</p>
             </div>
         </div>
     )
